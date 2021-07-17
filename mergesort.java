bc9a87eb90fa0aad364cbf227eb9b42cd6c02f12
brad182 <81612365+brad182@users.noreply.github.com>
2021-07-16 19:18:10 -0700

    Add files via upload

---
+++ mergesort.java
@@ -0,0 +1,76 @@
+import java.util.*;
+
+public class mergesort {
+
+    static int[] numbers;
+
+    public static void mergesort (int left, int right) {
+        if(left >= right) {
+            return;
+        }
+
+        int mid = (left + right) / 2;
+
+        mergesort(left, mid);
+        mergesort(mid + 1, right);
+
+        int i = left;
+        int j = mid + 1;
+
+        int k = 0;
+
+        int[] temp = new int[right - left + 1];
+        while (i <= mid && j <= right){
+            if (numbers[i] < numbers[j]) {
+                temp[k] = numbers[i];
+                i++;
+            }
+            else {
+                temp[k] = numbers[j];
+                j++;
+            }
+            k++;
+        }
+
+        /* the next two for loops are to fill up temp with the rest of the array that has not been completely added yet
+           if one array is already fully put into temp, the for loop will simply not execute
+         */
+
+        for (int x = i; x <= mid; x++) {
+            temp[k] = numbers[x];
+            k++;
+        }
+
+        for (int x = j; x <= right; x++) {
+            temp[k] = numbers[x];
+            k++;
+        }
+
+        int count = 0;
+        for(int x = left; x <= right; x++){
+            numbers[x] = temp[count];
+            count++;
+        }
+
+        for(int x = 0; x < numbers.length; x++){
+            if(x == numbers.length - 1){
+                System.out.println(numbers[x]);
+                break;
+            }
+            System.out.print(numbers[x] + " ");
+        }
+    }
+
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        numbers = new int[n];
+
+        for (int x = 0; x < n; x++) {
+            numbers[x] = scanner.nextInt();
+        }
+
+        mergesort(0, n - 1);
+    }
+}
---
+++ quicksort.java
@@ -0,0 +1,70 @@
+import java.util.*;
+
+public class quicksort {
+
+    static int[] numbers;
+
+    public static void quicksort(int left, int right) {
+        if (left >= right) {
+            return;
+        }
+
+        int pivot = numbers[right];
+
+        int i = left;
+        int j = right - 1;
+
+        while (true) {
+            while (numbers[i] < pivot && i < right) { // keep moving i forward until there is a number greater than or equal to the pivot
+                i++;
+            }
+
+            while (numbers[j] > pivot && j > 0) { // keep moving j backwards until there is a number less than or equal to the pivot
+                j--;
+            }
+
+            if(i >= j){
+                break;
+            }
+
+            int ithNumber = numbers[i];
+            int jthNumber = numbers[j];
+
+            numbers[i] = jthNumber;
+            numbers[j] = ithNumber;
+
+            i++;
+            j--;
+        }
+
+        int ithNumber = numbers[i];
+        int rthNumber = numbers[right];
+
+        numbers[i] = rthNumber;
+        numbers[right] = ithNumber;
+
+        for(int x = 0; x < numbers.length; x++){
+            if(x == numbers.length - 1){
+                System.out.println(numbers[x]);
+                break;
+            }
+            System.out.print(numbers[x] + " ");
+        }
+
+        quicksort(left, i - 1);
+        quicksort(i + 1, right);
+
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        numbers = new int[n];
+
+        for(int x = 0; x < n; x++){
+            numbers[x] = scanner.nextInt();
+        }
+
+        quicksort(0, n - 1);
+    }
+}
